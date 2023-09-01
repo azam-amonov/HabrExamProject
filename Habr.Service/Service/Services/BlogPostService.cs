a3e848e0cf3b4e2fc02408e00486e5d3c30ab2d7
Azam Amonov <azamamonov555@gmail.com>
2023-09-01 12:57:20 +0500

    Blog Post CRUD service is finished

--- Habr.Service/Service/Services/BlogPostService.cs
+++ Habr.Service/Service/Services/BlogPostService.cs
@@ -1,43 +1,95 @@
 using System.Linq.Expressions;
+using Habr.Service.DataAccses.Constans;
 using Habr.Service.Domain.Entities.BlogPosts;
+using Habr.Service.Service.Extentions;
 using Habr.Service.Service.Interfaces;
 
 namespace Habr.Service.Service.Services;
 
 public class BlogPostService : IBlogPostService
 {
+    private readonly string _blogPostDataPath = Constant.PostDataFile;
+    
+    public BlogPostService()
+    {
+        var source = File.ReadAllTextAsync(_blogPostDataPath);
+        if(string.IsNullOrEmpty(source.ToString()));
+            File.WriteAllTextAsync(_blogPostDataPath, "[]");
+    }
+    
     public IQueryable<BlogPost> GetAsync(Expression<Func<BlogPost>> expression)
     {
         throw new NotImplementedException();
     }
 
-    public Task<BlogPost> CreateAsync(BlogPost blogPost)
+    public async Task<BlogPost> CreateAsync(BlogPost blogPost)
     {
-        throw new NotImplementedException();
+        var blogPosts = await _blogPostDataPath.ReadJsonFromFileAsync<List<BlogPost>>();
+        
+        blogPosts.Add(blogPost);
+        await blogPosts.WriteToFileFromJsonAsync(_blogPostDataPath);
+        
+        return blogPost;
     }
 
-    public Task<BlogPost> UpdateAsync(BlogPost blogPost)
+    public async Task<BlogPost> UpdateAsync(BlogPost blogPost)
     {
-        throw new NotImplementedException();
+        var blogPosts = await _blogPostDataPath.ReadJsonFromFileAsync<List<BlogPost>>();
+        var blogPostToUpdate = blogPosts.FirstOrDefault(p => p.Id == blogPost.Id);
+        
+        if(blogPostToUpdate is null)
+            throw new ArgumentNullException(nameof(blogPostToUpdate), "This article is not published!");
+
+        blogPostToUpdate.Title = blogPost.Title;
+        blogPostToUpdate.Content = blogPost.Content;
+        blogPostToUpdate.UpdatedTime = DateTime.Now;
+
+        await blogPosts.WriteToFileFromJsonAsync(_blogPostDataPath);
+        return blogPost;
     }
 
-    public Task<BlogPost> DeleteAsync(BlogPost blogPost)
+    public async Task<BlogPost> DeleteAsync(BlogPost blogPost)
     {
-        throw new NotImplementedException();
+        var blogPosts = await _blogPostDataPath.ReadJsonFromFileAsync<List<BlogPost>>();
+        var blogPostToDelete = blogPosts.FirstOrDefault(p => p.Id == blogPost.Id);
+        
+        if(blogPostToDelete is null)
+            throw new ArgumentNullException(nameof(blogPostToDelete), "This article is not published!");
+        
+        blogPosts.Remove(blogPostToDelete);
+        await blogPosts.WriteToFileFromJsonAsync(_blogPostDataPath);
+        return blogPostToDelete;
     }
 
-    public Task<BlogPost> GetByIdAsync(int id)
+    public async Task<BlogPost> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var blogPosts = await _blogPostDataPath.ReadJsonFromFileAsync<List<BlogPost>>();
+        var existingBlogPost = blogPosts.FirstOrDefault(p => p.Id == id);
+        
+        if(existingBlogPost is null)
+            throw new ArgumentNullException(nameof(existingBlogPost), "This article is not published!");
+
+        return existingBlogPost;
     }
 
-    public Task<List<BlogPost>> GetByUserIdAsync(int id)
+    public async Task<List<BlogPost>> GetByUserIdAsync(int id) 
     {
-        throw new NotImplementedException();
+        var blogPosts = await _blogPostDataPath.ReadJsonFromFileAsync<List<BlogPost>>();
+        var userPosts = blogPosts.Any(p => p.UserId == id);
+
+        if (!userPosts)
+            throw new ArgumentException("The is no any blog post by this user id");
+
+        return blogPosts.Where(p => p.UserId == id).ToList();
     }
 
-    public Task<List<BlogPost>> GetAllAsync()
+    public async Task<List<BlogPost>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var blogPosts = await _blogPostDataPath.ReadJsonFromFileAsync<List<BlogPost>>();
+        
+        if (blogPosts.Count == 0)
+            throw new ArgumentException("The is no any blog post");
+        
+        return blogPosts;
     }
 }
