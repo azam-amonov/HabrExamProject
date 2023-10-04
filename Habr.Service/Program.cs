82192b463af338b694f8a330a1c9ea92153f526d
Azam Amonov <azamamonov555@gmail.com>
2023-10-04 16:52:59 +0500

    h

--- Habr.Service/Program.cs
+++ Habr.Service/Program.cs
@@ -6,13 +6,18 @@ using Habr.Service.Domain.Entities;
 using Habr.Service.Domain.Entities.BlogPosts;
 using Habr.Service.Domain.Entities.Comments;
 using Habr.Service.Domain.Enums;
+using Habr.Service.Service.Helpers;
 using Habr.Service.Service.Services;
 
-Console.WriteLine("Test");
-var userServive = new UserService();
-await userServive.CreateAsync(new User("John","john@example.com", "john@123", true, UserRole.Administrator,12));
-var postService = new BlogPostService();
-var commentsService = new CommentService();
+// Console.WriteLine("Test");
+// var userServive = new UserService();
+// await userServive.CreateAsync(new User("John","john@example.com", "john@123", true, UserRole.Administrator,12));
+// var postService = new BlogPostService();
+// var commentsService = new CommentService();
 
 // await postService.CreateAsync(new BlogPost(21, "John", "John"));
 // await commentsService.CreateAsync(new Comment(1,2,3,"comments"));
+if(Validator.IsValidEmail("exampleexample.com"))
+    Console.WriteLine("Email");
+else
+    Console.WriteLine("Please enter your email address");
